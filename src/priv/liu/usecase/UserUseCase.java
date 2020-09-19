9c0d9a4a045eeaf924282758aac49d6c090348ff
106598047 <1101105310@gm.kuas.edu.tw>
2020-09-19 21:59:21 +0800

    Refactor: Create UserUseCase with Mockito Test

---
+++ src/priv/liu/usecase/UserUseCase.java
@@ -0,0 +1,46 @@
+package priv.liu.usecase;
+
+import priv.liu.dao.UserDao;
+import priv.liu.entity.User;
+import priv.liu.factory.UserDaoFactory;
+
+public class UserUseCase {
+	private UserDao _userDao;
+
+	public UserUseCase() {
+		_userDao = new UserDaoFactory().createUserDao();
+	}
+	
+	// TODO: this is for testing
+	public UserUseCase(UserDao userDao) {
+		_userDao = userDao;
+	}
+
+	public boolean register(String username, String password) {
+		boolean isRegister = false;
+		if (isNullOrEmpty(username) || isNullOrEmpty(password))
+			isRegister = false;
+		else if (isUserExisting(username))
+			isRegister = false;
+		else
+			isRegister = _userDao.register(new User(username, password));
+		return isRegister;
+	}
+
+	public boolean login(String username, String password) {
+		boolean isLogin = false;
+		if (isNullOrEmpty(username) || isNullOrEmpty(password))
+			isLogin = false;
+		else
+			isLogin = _userDao.login(new User(username, password));
+		return isLogin;
+	}
+	
+	private boolean isNullOrEmpty(String input) {
+		return input == null || input.isEmpty();
+	}
+
+	private boolean isUserExisting(String username) {
+		return _userDao.isUserExisting(new User(username, null));
+	}
+}
---
+++ test/priv/liu/usecase/UserUseCaseTest.java
@@ -0,0 +1,114 @@
+package priv.liu.usecase;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import priv.liu.dao.UserDao;
+import priv.liu.entity.User;
+import priv.liu.factory.UserDaoFactory;
+
+public class UserUseCaseTest {
+	
+	private UserUseCase _userUseCase;
+	private UserDao _userDao;
+	
+	@Before
+	public void setUp() throws Exception {
+		_userDao = mock(UserDao.class);
+		_userUseCase = new UserUseCase(_userDao);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void isRegister_True_UsernamePasswordCorrect() {
+		doReturn(true).when(_userDao).register(any());
+		assertTrue(_userUseCase.register("tester", "secret"));
+	}
+	
+	@Test
+	public void isRegister_False_UsernameExisting() {
+		String username = "tester";
+		String password = "secret";
+		doReturn(true).when(_userDao).register(any());
+		assertTrue(_userUseCase.register(username, password));
+		verify(_userDao, times(1)).register(any());
+		doReturn(false).when(_userDao).register(any());
+		assertFalse(_userUseCase.register(username, password));
+	}
+	
+	@Test
+	public void isRegister_False_UsernameNull() {
+		assertFalse(_userUseCase.register(null, "secret"));
+	}
+	
+	@Test
+	public void isRegister_False_UsernameEmpty() {
+		assertFalse(_userUseCase.register("", "secret"));
+	}
+	
+	@Test
+	public void isRegister_False_PasswordNull() {
+		assertFalse(_userUseCase.register("tester", null));
+	}
+	
+	@Test
+	public void isRegister_False_PasswordEmpty() {
+		assertFalse(_userUseCase.register("tester", ""));
+	}
+	
+	@Test
+	public void isLogin_True_UsernamePasswordCorrect() {
+		String username = "tester";
+		String password = "secret";
+		_userUseCase.register(username, password);
+		doReturn(true).when(_userDao).login(any());
+		assertTrue(_userUseCase.login(username, password));
+	}
+	
+	@Test
+	public void isLogin_False_UsernameIncorrect() {
+		String username = "tester";
+		String password = "secret";
+		_userUseCase.register(username, password);
+		doReturn(false).when(_userDao).login(any());
+		String aNewUsername = "newTester";
+		assertFalse(_userUseCase.login(aNewUsername, password));
+	}
+	
+	@Test
+	public void isLogin_False_PasswordIncorrect() {
+		String username = "tester";
+		String password = "secret";
+		_userUseCase.register(username, password);
+		doReturn(false).when(_userDao).login(any());
+		String aNewPassword = "newSecret";
+		assertFalse(_userUseCase.login(username, aNewPassword));
+	}
+	
+	@Test
+	public void isLogin_False_UsernameNull() {
+		assertFalse(_userUseCase.login(null, "secret"));
+	}
+	
+	@Test
+	public void isLogin_False_UsernameEmpty() {
+		assertFalse(_userUseCase.login("", "secret"));
+	}
+	
+	@Test
+	public void isLogin_False_PasswordNull() {
+		assertFalse(_userUseCase.login("tester", null));
+	}
+	
+	@Test
+	public void isLogin_False_PasswordEmpty() {
+		assertFalse(_userUseCase.login("tester", ""));
+	}
+}
