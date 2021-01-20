86004a616618cdcbbd1625ab59f19b689538cdaa
106598047 <1101105310@gm.kuas.edu.tw>
2021-01-20 20:17:22 +0800

    Fix: users table in PostgreSql

--- sql-tables/PostgreSql/users.sql
+++ sql-tables/PostgreSql/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(45) NOT NULL CHECK (username <> ''),
-    password VARCHAR(45) NOT NULL CHECK (password <> ''),
+    password VARCHAR(65) NOT NULL CHECK (password <> ''),
     UNIQUE (username)
 );
 
